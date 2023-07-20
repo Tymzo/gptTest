fc2b9c0605c26aa71acdf2250e8eb7493f54a844
Tymzo <stefan.antnj@eduge.ch>
2023-07-20 14:14:51 +0200

    second commit

--- main.py
+++ main.py
@@ -10,31 +10,14 @@ app = Flask(__name__)
 
 os.environ["OPENAI_API_KEY"] = "023660028984431ba43b6df026a3170f"
 
-
-def split_text(text, chunk_size):
-    sentences = sent_tokenize(text)
-    chunks = []
-    current_chunk = []
-    words = text.split(" ")
-
-    for word in words:
-        current_chunk.append(word)
-        if len(current_chunk) == chunk_size:
-            
-        if len(current_chunk) + len(sentence.split()) <= chunk_size:
-            current_chunk.append(sentence)
-        else:
-            chunks.append(" ".join(current_chunk))
-            current_chunk = [sentence]
-
-    if current_chunk:
-        chunks.append(" ".join(current_chunk))
-
-    return chunks
+openai.api_type = "azure"
+openai.api_base = "https://z-openai-openai4is-dev-frc.openai.azure.com/"
+openai.api_version = "2023-03-15-preview"
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
+@app.route('/task/<action>', methods=['POST'])
+def upload_file(action):
     if 'file' not in request.files:
         return 'No file part in the request', 400
 
@@ -45,65 +28,90 @@ def upload_file():
 
     save_path = os.path.join('./tmp', file.filename)
     file.save(save_path)
-    print_content(save_path)
 
-    return 'File uploaded and saved successfully', 200
+    res = upload_clean(save_path)
+    res_clean_file = clean_file(res)
+
+    if action == 'upload_clean':
+        return res
+    elif action == 'clean_file':
+        return res_clean_file
+    else:
+        return "Action not found", 404
 
 
-def print_content(file_path):
-    with open(file_path, 'r') as f:
+def split_text(text, chunk_size):
+    words = text.split()
+    print(len(words))
+    chunks = []
+
+    for i in range(0, len(words), chunk_size):
+        chunk = ' '.join(words[i: i + chunk_size])
+        chunks.append(chunk)
+
+    return chunks
+
+
+def upload_clean(file_path):
+    with open(file_path, 'r', encoding='ISO-8859-1') as f:
         file_content = f.read()
-        # print('File content:')
-        # print(file_content)
-
-    openai.api_type = "azure"
-    openai.api_base = "https://z-openai-openai4is-dev-frc.openai.azure.com/"
-    openai.api_version = "2023-03-15-preview"
-    openai.api_key = os.getenv("OPENAI_API_KEY")
 
     chunk_size = 1000
     text_chunks = split_text(file_content, chunk_size)
 
     gpt_responses = []
-
-
-
-    for i, chunk in enumerate(text_chunks):
-        print("********************************************")
-        print(len(chunk))
-        print(chunk[:3])
-        if len(chunk) < 1005:
-            response = openai.ChatCompletion.create(
-                engine="GPT-4",
-                messages=[
-                    {
-                        "role": "user",
-                        "content": f"Voici le contenu du fichier (partie {i + 1}):"
-                    },
-                    {
-                        "role": "user",
-                        "content": chunk
-                    },
-                    {
-                        "role": "user",
-                        "content": "Liste de termes et occurrences, nettoyer bruit, termes inutiles ou non pertinents."
-                    }
-                ],
-                temperature=0.7,
-                max_tokens=100,
-                top_p=0.95,
-                frequency_penalty=0,
-                presence_penalty=0,
-                stop=None
-            )
-
+    for chunk in text_chunks:
+        response = openai.ChatCompletion.create(
+            engine="GPT-4",
+            messages=[
+                {
+                    "role": "system",
+                    "content": f"You are a professional terminologist. Your job is to extract important terms from a"
+                               f" given text. limit yourself to 5 terms per a given text"
+                               f"Make sure to return them as a list of terms highly sophisticated"
+                },
+                {
+                    "role": "user",
+                    "content": chunk
+                }
+            ],
+            temperature=0.6,
+            max_tokens=50,
+            top_p=0.95,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=None
+        )
         gpt_responses.append(response.choices[0]['message']['content'])
+    return gpt_responses
 
-    print("Réponses GPT pour chaque morceau :")
-    for i, response in enumerate(gpt_responses):
-        print(f"Réponse {i + 1}:")
-        print(response)
-        print()
+
+def clean_file(response):
+    gpt_responses = []
+    to_clean = '\n'.join(response)
+    response = openai.ChatCompletion.create(
+        engine="GPT-4",
+        messages=[
+            {
+                "role": "system",
+                "content": f"You are a professional terminologist. Your job is to clean a list of terms keeping "
+                           f"only essential and important terms from a given list of terms"
+                           f"Make sure to return them as a list of terms highly sophisticated"
+            },
+            {
+                "role": "user",
+                "content": to_clean
+            }
+        ],
+        temperature=0.6,
+        max_tokens=300,
+        top_p=0.95,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None
+    )
+    gpt_responses.append(response.choices[0]['message']['content'])
+    return gpt_responses
 
 
 if __name__ == '__main__':
