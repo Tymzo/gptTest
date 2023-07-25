d9055bd3158f832170b8d1f5c88eaab0635e97f3
Tymzo <Stefan.antnj@eduge.ch>
2023-07-25 11:15:46 +0200

    1st commit

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
 import openai
 import os
+import json
 
+os.environ["REQUESTS_CA_BUNDLE"] = 'C:\\Users\\antonije\\Downloads\\ITU_Root_Authority.crt'
+os.environ["SSL_CERT_FILE"] = 'C:\\Users\\antonije\\Downloads\\ITU_Root_Authority.crt'
 
 app = Flask(__name__)
 
@@ -14,27 +17,31 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 @app.route('/task/<action>', methods=['POST'])
-def upload_file(action):
+def upload_files(action):
     if 'file' not in request.files:
         return 'No file part in the request', 400
 
-    file = request.files['file']
+    files = request.files.getlist('file')
+    results = []
 
-    if file.filename == '':
-        return 'No file selected for uploading', 400
+    for file in files:
+        if file.filename == '':
+            return 'No file selected for uploading', 400
 
-    save_path = os.path.join('./tmp', file.filename)
-    file.save(save_path)
+        save_path = os.path.join('./tmp', file.filename)
+        file.save(save_path)
 
-    res = upload_clean(save_path)
-    res_clean_file = clean_file(res)
+        res_upload_clean = upload_clean(save_path)
+        res_clean_file = clean_file(res_upload_clean)
 
-    if action == 'upload_clean':
-        return res
-    elif action == 'clean_file':
-        return res_clean_file
-    else:
-        return "Action not found", 404
+        if action == 'upload_clean':
+            results.append(jsonify(response=res_upload_clean, status=200))
+        elif action == 'clean_file':
+            results.append(jsonify(response=res_clean_file, status=200))
+        else:
+            return "Action not found", 404
+
+    return jsonify(results)
 
 
 def split_text(text, chunk_size):
@@ -50,10 +57,10 @@ def split_text(text, chunk_size):
 
 
 def upload_clean(file_path):
-    with open(file_path, 'r', encoding='ISO-8859-1') as f:
+    with open(file_path, 'r', encoding='iso-8859-1') as f:
         file_content = f.read()
 
-    chunk_size = 1000
+    chunk_size = 500
     text_chunks = split_text(file_content, chunk_size)
 
     gpt_responses = []
@@ -84,16 +91,16 @@ def upload_clean(file_path):
 
 
 def clean_file(response):
-    gpt_responses = []
     to_clean = '\n'.join(response)
     response = openai.ChatCompletion.create(
         engine="GPT-4",
         messages=[
             {
                 "role": "system",
-                "content": f"You are a professional terminologist. Your job is to clean a list of terms keeping "
-                           f"only essential and important terms from a given list of terms"
-                           f"Make sure to return them as a list of terms highly sophisticated"
+                "content": "You are a professional terminologist. Your job is to clean a list of terms keeping "
+                           "only essential and important terms from a given list of terms"
+                           "Make sure to return them as json format {'cleaned_terms': [term1, term2 ...]} to be parsed"
+                           " in the frontend, limit yourself to 5 terms "
             },
             {
                 "role": "user",
@@ -101,14 +108,15 @@ def clean_file(response):
             }
         ],
         temperature=0.6,
-        max_tokens=300,
+        max_tokens=100,
         top_p=0.95,
         frequency_penalty=0,
         presence_penalty=0,
         stop=None
     )
-    gpt_responses.append(response.choices[0]['message']['content'])
-    return gpt_responses
+    print(response.choices[0]['message']['content'])
+    res = json.loads(response.choices[0]['message']['content'])
+    return jsonify(response=res, status=200)
 
 
 if __name__ == '__main__':
